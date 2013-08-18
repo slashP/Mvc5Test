3735debbabfc8c16a75e416e87cc2e65db0c1daa
Per-Kristian Helland <perkristianhelland@gmail.com>
2013-08-18 20:02:40 +0200

    Initial commit

---
+++ Mvc5Test/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Owin;
+
+namespace Mvc5Test
+{
+    public partial class Startup 
+    {
+        public void Configuration(IAppBuilder app) 
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
